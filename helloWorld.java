cb3c00db8ebe401b7b514f183228714318ad77d8
lebao0112 <113030053+lebao0112@users.noreply.github.com>
2023-07-18 09:21:42 +0700

    them loi chao

--- helloWorld.java
+++ helloWorld.java
@@ -1,9 +1,9 @@
 package demogithub;
 
 public class helloWorld{
-
+    String sayHello = "Xin chao";
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println(sayHello);
     }
 }
